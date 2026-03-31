5af8e0dd63862c741b2461991735e49b7c63ce38
ymq <2179818967@qq.com>
2026-03-31 18:30:12 +0800

    print 3 times

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");  // 新增的第二行打印
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
